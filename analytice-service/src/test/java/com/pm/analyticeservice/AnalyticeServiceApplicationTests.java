602c1399fc060ecc6eb5a8be50876188426d13f9
Balram-Ishwar <ishwarbalram22@gmail.com>
2025-09-10 11:59:10 +0530

    ADD Kafka Consumer: Analytics Service

---
+++ analytice-service/src/test/java/com/pm/analyticeservice/AnalyticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.analyticeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
